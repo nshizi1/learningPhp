81058baf5f186743be032015b6be5b0b61cf742f
Wilson Nshizirungu <wilsonnshi1@gmail.com>
2024-03-05 21:01:58 +0200

    learning

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+
+    // ABout JSON format
+    // header("Content-Type: application/json");
+    // $array = array("rice", "milk", "juice", "beans");
+    // $array = array(
+    //     "success" => true,
+    //     "message" => "Hello World"
+    // );
+    // $array = array(
+    //     array(
+    //         "id" => 1,
+    //         "name" => "Wilson Nshizirungu"
+    //     ),
+    //     array(
+    //         "id" => 2,
+    //         "name" => "Shema Christian"
+    //     ),
+    //     array(
+    //         "id" => 3,
+    //         "name" => "Ineza Carine"
+    //     ),
+    //     array(
+    //         "id" => 4,
+    //         "name" => "Nshuti Lancelot"
+    //     ),
+    // );
+    // $products = json_encode($array, JSON_PRETTY_PRINT);
+    // echo $products;
+
+    // PHP patterns
+
+    // for($i = 1; $i<=5; $i++){
+    //         echo $i." ";
+    // }
+
+    // $colors = array("red", "blue", "green", "yellow","pink", "purple");
+
+    // foreach($colors as $color){
+    //     echo $color."</br>";
+    // }
+
+    // multiplication table from 1 to 5 (1x1 to 5x5)
+
+    // creating a table
+    // echo "<table border='1'>";
+    // for($i = 1; $i<=5; $i++){
+    //     echo "<tr>";
+    //     for($j = 1; $j <=5; $j++){
+    //         echo "<td> $i x $j=".$i*$j."</td>";
+    //     }
+    //     echo "</tr>";
+    // }
+    // echo "</table>";
